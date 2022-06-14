3216903d1785e92b4330b6353e14e515d3b458ce
Steven Saunders <steven@supersuperstore.com>
2022-06-14 11:09:10 +0100

    fix: fixed issues when site viewed in light mode

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import LatestPosts from '@NonoviumUI/blogUI/LatestPosts';
+import Button from '@NonoviumUI/buttons/Button';
 import Block from '@NonoviumUI/containers/Block';
 import DevIconStack from '@NonoviumUI/devLang/DevIconStack';
 import HeaderFooter from '@NonoviumUI/layout/HeaderFooter';
@@ -24,7 +25,7 @@ export default function Page({ blogPosts }: props) {
       <div id='content'>
         <section className='prose mx-auto mt-16 max-w-7xl px-4 text-center dark:prose-invert'>
           <div className='mx-auto flex flex-wrap-reverse items-center'>
-            <h1 className='txt-shdw-distant dark:txt-shdw-distant-dark px-7 text-2xl leading-tight sm:text-4xl md:max-w-[70%] md:leading-normal'>
+            <h1 className='dark:txt-shdw-distant-dark px-7 text-2xl leading-tight drop-shadow-md sm:text-4xl md:max-w-[70%] md:leading-normal'>
               I&apos;m{' '}
               <span className='text-primary-500 dark:text-primary-400'>
                 Steven Saunders
@@ -32,7 +33,7 @@ export default function Page({ blogPosts }: props) {
               . I&apos;m a full stack developer and software engineer with a
               passion for coding.
             </h1>
-            <div className='mx-auto block min-w-[40%] py-7 px-3 md:min-w-[30%]'>
+            <div className='mx-auto block min-w-[40%] py-7 px-3 drop-shadow-md md:min-w-[30%]'>
               <Image
                 src='https://s3.pub1.infomaniak.cloud/object/v1/AUTH_598753f3935548c6a1ca8f065e534ac1/supersuperdev-pub/img/profile1.png'
                 alt='Steven Saunders'
@@ -46,7 +47,7 @@ export default function Page({ blogPosts }: props) {
         </section>
 
         <section className='prose mx-auto my-9 max-w-7xl flex-col items-center p-9 text-center dark:prose-invert'>
-          <h1 className='txt-shdw-distant dark:txt-shdw-distant-dark px-7 text-2xl leading-tight sm:text-4xl md:leading-normal'>
+          <h1 className='dark:txt-shdw-distant-dark px-7 text-2xl leading-tight drop-shadow-md sm:text-4xl md:leading-normal'>
             I build scalable, performant, and accessible web applications.
           </h1>
           <div className='min-h-[400px] py-20'>
@@ -61,14 +62,14 @@ export default function Page({ blogPosts }: props) {
             <DevIconInline devTech='all' iconFormat='DevIconWordUrl' />
           </div> */}
         </section>
-        <section className='my-9 w-screen skew-y-6 flex-col items-center justify-items-center bg-gradient-to-r from-purple-900 to-red-900 bg-cover bg-fixed p-9 text-center'>
+        <section className='my-9 w-screen skew-y-6 flex-col items-center justify-items-center bg-gradient-to-r from-purple-900 to-red-900 bg-cover bg-fixed p-12 text-center'>
           <div className='mx-auto -skew-y-6 py-12'>
-            <h1 className='txt-shdw-distant dark:txt-shdw-distant-dark justify-items-center px-7 text-2xl leading-tight sm:text-4xl md:leading-normal'>
+            <h1 className='dark:txt-shdw-distant-dark justify-items-center px-7 text-2xl leading-tight text-white drop-shadow-md sm:text-4xl md:leading-normal'>
               This site is built with....
             </h1>
 
             <div className='container mx-auto'>
-              <div className='grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4'>
+              <div className='grid grid-cols-1 gap-6 md:grid-cols-2'>
                 <div className='flex justify-center rounded-xl border-2 border-primary-300 p-6 text-6xl'>
                   <DevIconStack
                     devTech={['TypeScript', 'JavaScript', 'React', 'Nextjs']}
@@ -76,34 +77,28 @@ export default function Page({ blogPosts }: props) {
                     title='Frontend'
                     iconWidth={96}
                     showTitle='never'
+                    className='text-white'
                   />
                 </div>
                 <div className='flex justify-center rounded-xl border-2 border-primary-300 p-6 text-6xl'>
-                  <DevIconStack
-                    devTech={['Python', 'Django']}
-                    iconFormat='DevIconWordUrl'
-                    title='Backend'
-                    iconWidth={96}
-                    showTitle='never'
-                  />
-                </div>
-                <div className='flex justify-center rounded-xl border-2 border-primary-300 p-6 text-6xl'>
-                  <DevIconStack
-                    devTech={['PostgreSQL', 'Redis']}
-                    iconFormat='DevIconWordUrl'
-                    title='Database'
-                    iconWidth={96}
-                    showTitle='never'
-                  />
-                </div>
-                <div className='flex justify-center rounded-xl border-2 border-primary-300 p-6 text-6xl'>
-                  <DevIconStack
-                    devTech={['Python', 'Django', 'PostgreSQL', 'Redis']}
-                    iconFormat='DevIconWordUrl'
-                    title='Video Encoding'
-                    iconWidth={96}
-                    showTitle='never'
-                  />
+                  <div className='basis-full'>
+                    <DevIconStack
+                      devTech={['Python', 'Django']}
+                      iconFormat='DevIconWordUrl'
+                      title='Backend'
+                      iconWidth={96}
+                      showTitle='never'
+                      className='text-white'
+                    />
+                    <DevIconStack
+                      devTech={['PostgreSQL', 'Redis']}
+                      iconFormat='DevIconWordUrl'
+                      title='Database'
+                      iconWidth={96}
+                      showTitle='never'
+                      className='text-white'
+                    />
+                  </div>
                 </div>
               </div>
             </div>
@@ -111,11 +106,18 @@ export default function Page({ blogPosts }: props) {
         </section>
         {blogPosts && (
           <section className='prose mx-auto my-9 max-w-7xl flex-col items-center p-9 text-center dark:prose-invert'>
-            <h1 className='txt-shdw-distant dark:txt-shdw-distant-dark px-7 text-2xl leading-tight sm:text-4xl md:leading-normal'>
-              Read my <Link href='/blog'>blog...</Link>
-            </h1>
+            <h2 className='dark:txt-shdw-distant-dark px-7 text-2xl leading-tight drop-shadow-md sm:text-4xl md:leading-normal'>
+              Checkout my Blog
+            </h2>
             <Block className='my-4 py-4'>
               <LatestPosts posts={blogPosts} showNumPosts={4} />
+            </Block>
+            <Block className='my-4 py-4 text-right'>
+              <Link href='/blog' passHref>
+                <a>
+                  <Button variant='outline'>More Posts...</Button>
+                </a>
+              </Link>
             </Block>
           </section>
         )}
